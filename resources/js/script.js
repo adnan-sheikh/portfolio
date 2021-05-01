03609ca5a2017245d63d2c9ad68778ad7394da08
mech-coder <40724165+adnan-sheikh@users.noreply.github.com>
2021-05-01 13:56:00 +0530

    adding view buttons interactivity for about sections

---
+++ resources/js/script.js
@@ -0,0 +1,14 @@
+const viewMoreBtn = document.querySelector('.view-more-js');
+const viewLessBtn = document.querySelector('.view-less-js');
+const moreAboutContent = document.querySelector('.full-about');
+
+viewMoreBtn.addEventListener('click', () => {
+    viewMoreBtn.style.display = 'none';
+    moreAboutContent.style.display = 'block';
+});
+
+viewLessBtn.addEventListener('click', () => {
+    moreAboutContent.style.display = 'none';
+    viewMoreBtn.style.display = 'inline-block';
+});
+
