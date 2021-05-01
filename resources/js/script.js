84fab1062d41775897c262c6d72ca2c0752ce749
mech-coder <40724165+adnan-sheikh@users.noreply.github.com>
2021-05-01 15:55:14 +0530

    adding hamburger menu and related js

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,3 +1,4 @@
+// View button script for About section
 const viewMoreBtn = document.querySelector('.view-more-js');
 const viewLessBtn = document.querySelector('.view-less-js');
 const moreAboutContent = document.querySelector('.full-about');
@@ -12,3 +13,18 @@ viewLessBtn.addEventListener('click', () => {
     viewMoreBtn.style.display = 'inline-block';
 });
 
+// Hamburger menu script
+
+const menuBtn = document.querySelector('.menu');
+let isMenuOpen = false;
+
+menuBtn.addEventListener('click', () => {
+    if (!isMenuOpen) {
+        menuBtn.classList.add('open');
+        isMenuOpen = true;
+    } else {
+        isMenuOpen = false;
+        menuBtn.classList.remove('open');
+    }
+    console.log(menuBtn.classList);
+});
