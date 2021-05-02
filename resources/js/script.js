5589f260a2ce775c5d29ae80446404b9f9dd8c7c
mech-coder <40724165+adnan-sheikh@users.noreply.github.com>
2021-05-02 12:36:56 +0530

    add website description, favicon and remove unnecessary divs

--- resources/js/script.js
+++ resources/js/script.js
@@ -19,8 +19,6 @@ const menuBtn = document.querySelector('.menu');
 const menuContent = document.querySelector('.menu-content');
 const links = document.querySelectorAll('.menu-content li');
 
-console.log(links);
-
 menuBtn.addEventListener('click', () => {
     menuBtn.classList.toggle('open');
     menuContent.classList.toggle('show');
